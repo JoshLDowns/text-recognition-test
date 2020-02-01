9db719298bfdd4722e638ee9e795f603cddde9ac
JoshLDowns <josh.lloyd.downs@gmail.com>
2020-02-01 10:44:03 -0500

    reworked code using lodash and tokenizer

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const word = process.argv[2];
-
-const natural = require('natural');;
-const wordnet = new natural.WordNet();
-
-
-
-
-//wordnet.lookup(word, function(details) {
-//    details.forEach(function(detail) {
-//        console.log("------------------------------");
-//        console.log("Definition: " + detail.def);
-//        console.log("Synonyms: " + detail.synonyms);
-//        console.log("POS: " + detail.pos);
-//
-//        // Display examples, if available
-//        detail.exp.forEach(function(example) {
-//            console.log("EXAMPLE: " + example);
-//        });
-//        console.log("------------------------------");
-//    });
-//});
-
 //let s = "Inspect the room";
 
 //function resultCheck(sentence) {
@@ -41,33 +18,69 @@ const wordnet = new natural.WordNet();
 //let newOb = resultCheck(s);
 //console.log(newOb);
 
-let s = "Inspect the room";
+//let s = "Inspect the room";
+//
+//function resultCheck(sentence) {
+//    a = sentence.split(' ');
+//    let sObj = {};
+//    let p = new Promise((resolve, reject) => {
+//        a.forEach(word => {
+//            sObj[word] = [];
+//            wordnet.lookupSynonyms(word, function (results) {
+//                results.forEach(function (result) {
+//                    sObj[word].push(result.synonyms);
+//                });
+//                sObj[word] = sObj[word].join().split(',');
+//                //console.log(sObj);
+//            });
+//        });
+//        resolve(sObj);
+//    });
+//    console.log(sObj);
+//    p.then((object) => {
+//        return object;
+//    })
+//    //return sObj;
+//}
+//
+//function getSym() {
+//    let symObj = resultCheck(s);
+//    return symObj;
+//};
+//console.log(getSym());
 
-function resultCheck(sentence) {
-    a = sentence.split(' ');
-    let sObj = {};
-    let p = new Promise((resolve, reject) => {
-        a.forEach(word => {
-            sObj[word] = [];
-            wordnet.lookupSynonyms(word, function (results) {
-                results.forEach(function (result) {
-                    sObj[word].push(result.synonyms);
-                });
-                sObj[word] = sObj[word].join().split(',');
-                //console.log(sObj);
-            });
-        });
-        resolve(sObj);
-    });
-    console.log(sObj);
-    p.then((object) => {
-        return object;
-    })
-    //return sObj;
+const natural = require('natural');
+const _ = require('lodash')
+
+
+const wordnet = new natural.WordNet();
+const tokenizer = new natural.WordTokenizer();
+
+
+const sent = "Welcome to callback hell."
+const setSyns = {}
+
+const handleLookup = (word, syns) => {
+    setSyns[word] = syns
 }
 
-function getSym() {
-    let symObj = resultCheck(s);
-    return symObj;
-};
-console.log(getSym());
+const doLast = () => {
+    console.log(setSyns)
+}
+
+// callback to wordnet.lookup gets passed an array of objects
+// one property of which is `synonyms`
+
+// also, use a tokenizer
+
+tokenizer.tokenize(sent).forEach((word, idx, words) => {
+    wordnet.lookup(word, x => {
+        synsListOfLists = _.flatMapDeep(x, y => y.synonyms)
+        handleLookup(word, _.uniq(synsListOfLists))
+        if (idx == words.length - 1) {
+            doLast()
+        }
+    })
+})
+
+console.log(setSyns)  // logs {} because this is executed before the loop is done
